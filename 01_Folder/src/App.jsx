e59ab24f731e93aad6d04b610b01a070cd37b6b8
faseehullah khan <143412443+faseehullahkhan@users.noreply.github.com>
2025-12-24 02:31:13 +0500

    First Commit

---
+++ 01_Folder/src/App.jsx
@@ -0,0 +1,17 @@
+// react arrow function export component - rafce
+import React from "react";
+
+const App = () => {
+  return(
+    <>
+      <div id="faseeh">
+        <h1>faseeh</h1>
+      </div>
+      <div id="ahmed">
+        <h1>ahmed</h1>
+      </div>
+    </>
+  )
+};
+
+export default App;
