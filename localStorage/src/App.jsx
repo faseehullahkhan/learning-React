21ff3faa2d3460a5dc79de5ce8e770d2c143de1f
faseehullah khan <143412443+faseehullahkhan@users.noreply.github.com>
2026-01-27 19:21:04 +0500

    learned about local Storage and the way to send the data on the storage like objects and learn how we can convert them using JSON.parse() and JSON.stringify() , methods.

---
+++ localStorage/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  // localStorage.clear()
+  // localStorage.setItem('user'  , 'faseeh')
+  // localStorage.getItem('user')
+  // localStorage.removeItem('user')
+  // JSON.parse()
+  // JSON.stringify()
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
